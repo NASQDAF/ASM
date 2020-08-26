0bec6ff20904ccabf4e2dbe9cadee92f8d4153b4
NASQDAF <69731829+NASQDAF@users.noreply.github.com>
2020-08-26 21:18:48 +0300

    BSF
    Bit Scan Forward

--- BSF/bsf.asm
+++ BSF/bsf.asm
@@ -1,9 +1,9 @@
 ;Shift the contents of register bx to the right so that bit zero becomes one
 .386
-        mov bx, 0002h; bx = 0000 0010b
+        mov bx, 0002h	;bx = 0000 0010b
 ...
-        bsf cx, bx; cx = 0001h
+        bsf cx, bx  	;cx = 0001h
         jz null
-        shr bx, cl; bx = 0000 0001b
+        shr bx, cl  	;bx = 0000 0001b
 ...
 null:
